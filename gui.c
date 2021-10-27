96e45654939455a7eceef4fbf8f2ddfcad9f156c
Lukasz Tracz <lukasztracz195@gmail.com>
2021-10-28 01:13:18 +0200

    Added gui implementation

---
+++ gui.c
@@ -0,0 +1,106 @@
+#include <gtk/gtk.h>
+
+//GLOBAL VARIABLES
+const int WIDTH_OF_WINDOW = 500;
+const int HEIGHT_OF_WINDOW = 500;
+const char* NAME_OF_WINDOW = "Boguslaw Kowalski REGEX";
+const char* CONTENT_OF_BANNER = "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n\
+      Analizator skladniowy LL(1) wykozystujacy rozbior        \n\
+      generacyjny zstepujacy z wyprzedzeniem o jeden symbol    \n\
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n\
+      KAZDE WYRAZENIE MUSI BYC ZAKONCZONE ZNAKIEM ';'         \n\
+ Wprowadz w poniższe pole wyrazenie mateamtyczne do walidacji\n\
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n";
+const char* PLACEHOLDER_ON_EXPRESSION_ENTRY = "This is please for enter your mathemathic formula";
+const char* DEFAULT_RESULT_MESSAGE = "This is please on result of validation";
+const char* EMPTY_STR = "";
+
+//Uchwyty na widgety
+GtkWidget *window;
+GtkWidget *validation_button;
+GtkWidget *reset_button;
+GtkWidget *baner;
+GtkWidget *label_on_result_message;
+GtkWidget *vbox;
+GtkWidget *entry_for_input_expression;
+
+//Funkcje do inicjowania i obsługi widgetów
+void validation(GtkWidget *widget, gpointer data) {
+  const gchar* expression = gtk_entry_get_text(GTK_ENTRY(entry_for_input_expression));
+	g_print("Expression = %s\n", expression);
+}
+
+GtkWidget* init_window(){
+	GtkWidget* window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+  gtk_window_set_default_size(GTK_WINDOW(window), WIDTH_OF_WINDOW, HEIGHT_OF_WINDOW);
+  gtk_window_set_title(GTK_WINDOW(window), NAME_OF_WINDOW);
+  gtk_container_set_border_width(GTK_CONTAINER(window), 15);
+  return window;
+}
+
+GtkWidget* init_baner(){
+	GtkWidget *baner;
+	baner = gtk_label_new(CONTENT_OF_BANNER);
+	gtk_label_set_justify(GTK_LABEL(baner), GTK_JUSTIFY_CENTER);
+  return baner;
+}
+
+GtkWidget* init_entry_for_input_expression(){
+	GtkWidget *entry_for_input_expression;
+	entry_for_input_expression = gtk_entry_new();
+	gtk_entry_set_placeholder_text(GTK_ENTRY(entry_for_input_expression), PLACEHOLDER_ON_EXPRESSION_ENTRY);
+	gtk_entry_set_visibility(GTK_ENTRY(entry_for_input_expression), TRUE);
+	return entry_for_input_expression;
+}
+
+GtkWidget* init_label_on_result_of_validation(){
+	GtkWidget *label_on_result_message;
+	label_on_result_message = gtk_label_new(DEFAULT_RESULT_MESSAGE);
+	gtk_label_set_justify(GTK_LABEL(label_on_result_message), GTK_JUSTIFY_CENTER);
+  return label_on_result_message;
+}
+
+void reset_form() {
+	g_print("RESET\n");
+	gtk_entry_set_text(GTK_ENTRY(entry_for_input_expression),EMPTY_STR);
+	gtk_entry_set_text(GTK_ENTRY(entry_for_input_expression),EMPTY_STR);
+	gtk_label_set_text(GTK_LABEL(label_on_result_message),DEFAULT_RESULT_MESSAGE);
+}
+
+int main(int argc, char *argv[]) {
+    
+
+  gtk_init(&argc, &argv);
+	
+	//Initailizacja obiektów view
+  window = init_window();
+  baner = init_baner();
+  label_on_result_message = init_label_on_result_of_validation();
+  entry_for_input_expression = init_entry_for_input_expression();
+  vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+  validation_button = gtk_button_new_with_label("Validate");
+  reset_button = gtk_button_new_with_label("Reset Form");
+  
+  
+  //Pozycjonowanie elementów
+  gtk_container_add(GTK_CONTAINER(window), vbox);
+  gtk_box_pack_start(GTK_BOX(vbox), baner, TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(vbox), entry_for_input_expression, TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(vbox), validation_button, TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(vbox), validation_button, TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(vbox), label_on_result_message, TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(vbox), reset_button, TRUE, TRUE, 0);
+  
+  //Przypisanie akcji do widgetów
+  g_signal_connect(G_OBJECT(validation_button), "clicked", G_CALLBACK(validation), NULL);
+  
+    g_signal_connect(G_OBJECT(reset_button), "clicked", G_CALLBACK(reset_form), NULL);
+    
+  //Pokazanie okna 
+  g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+  gtk_widget_show_all(window);
+  gtk_main();
+
+  return 0;
+}
