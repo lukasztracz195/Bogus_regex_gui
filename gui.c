fb7097c3c07f3a3bb2e133128853ebfb30b744ea
Lukasz Tracz <lukasztracz195@gmail.com>
2021-10-29 19:56:06 +0200

    Probably app is finished

--- gui.c
+++ gui.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-//GLOBAL VARIABLES
+//ZMIENNE GLOBALNE
 const int WIDTH_OF_WINDOW = 500;
 const int HEIGHT_OF_WINDOW = 500;
 const char* NAME_OF_WINDOW = "Boguslaw Kowalski REGEX";
@@ -16,10 +16,88 @@ const char* CONTENT_OF_BANNER = "+++++++++++++++++++++++++++++++++++++++++++++++
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n";
 const char* PLACEHOLDER_ON_EXPRESSION_ENTRY = "This is please for enter your mathemathic formula";
 const char* DEFAULT_RESULT_MESSAGE = "This is please on result of validation";
-const char* DEFAULT_ERROR_MESSAGE = "This is please on error of validation";
+const char* DEFAULT_ERROR_MESSAGE = "";
 const char* EMPTY_STR = "";
 const char* SUCCESS_MESSAGE = "Wyrazenie jest zgodne z gramatyka\n";
 const char* FAILED_MESSAGE = "Wyrazenie jest NIE zgodne z gramatyka\n";
+
+//PARAMETRY PROGRAMU
+const int ROZMIAR_BUFFORA = 100;
+char BUFFOR[100];
+char BUFFOR_OF_ERROR[100];
+char PUSTY_SYMBOL = '\0';
+const int INDEKS_W_TABLICY_ASCI_ZNAKU_NOWEJ_LINI = 10;
+char biezacy_symbol;
+char BLEDNY_SYMBOL = 0;
+bool BLAD = false;
+int i = 0;
+int indeks = 0;
+enum Pierwsze {
+	PierwszeProdukcjaS = 0,
+	PierwszeProdukcjaZ = 1,
+	PierwszeProdukcjaW = 2,
+	PierwszeProdukcjaP = 3,
+	PierwszeProdukcjaR = 4,
+	PierwszeProdukcjaL = 5,
+	PierwszeProdukcjaC = 6,
+	PierwszeProcukcjaO = 7,
+	PierwszeProdukcjaWprim = 8,
+	PierwszeProdukcjaRprim = 9,
+	PierwszeProdukcjaLprim = 10
+};
+
+char* NAME_OF_PRODUCTIONS[] = {
+"FIRST_PRODUCTION_S",
+"FIRST_PRODUCTION_Z",
+"FIRST_PRODUCTION_W",
+"FIRST_PRODUCTION_P",
+"FIRST_PRODUCTION_R",
+"FIRST_PRODUCTION_L",
+"FIRST_PRODUCTION_C",
+"FIRST_PRODUCTION_O",
+"FIRST_PRODUCTION_Wprim",
+"FIRST_PRODUCTION_Rprim",
+"FIRST_PRODUCTION_Lprim"
+};
+
+//ZDEFINIOWANE ZBIORY SYMBOLI NALEZACYCH DO ZBIOROW FIRST DLA POSZCZEGOLNYCH PRODUKCJI
+char FIRST_PRODUCTION_S[] = { '(','0','1','2','3','4','5','6','7','8','9' };
+char FIRST_PRODUCTION_Z[] = { '(','0','1','2','3','4','5','6','7','8','9','\0' };
+char FIRST_PRODUCTION_W[] = { '(','0','1','2','3','4','5','6','7','8','9' };
+char FIRST_PRODUCTION_P[] = { '(','0','1','2','3','4','5','6','7','8','9' };
+char FIRST_PRODUCTION_R[] = { '0','1','2','3','4','5','6','7','8','9' };
+char FIRST_PRODUCTION_L[] = { '0','1','2','3','4','5','6','7','8','9' };
+char FIRST_PRODUCTION_C[] = { '0','1','2','3','4','5','6','7','8','9' };
+char FIRST_PRODUCTION_O[] = { '*','+','-',':','^' };
+char FIRST_PRODUCTION_Wprim[] = { '*','+','-',':','^','\0' };
+char FIRST_PRODUCTION_Rprim[] = { '.',' ' };
+char FIRST_PRODUCTION_Lprim[] = { '0','1','2','3','4','5','6','7','8','9','\0' };
+
+//FUNCKJE POMOCNICZE
+int rozmiarTablicyZnakow(char **array);
+void wyczyscBuffor();
+void wyczyscBufforError();
+bool znajdzSymbolWProdukcji(char symbol, int production);
+char nastepnySymbolZInkrementacja();
+char nastepnySymbolBezInkrementacja();
+void sygnalizujBlad();
+void wczytaj();
+void print_result(bool wynik);
+bool sprawdzWyrazenie(char* expression);
+void reset_form();
+void reset_data_to_validation();
+//FUNKCJE REALIZUJACE OBSLUGE ZNAKU NALEZACEGO DO DANEJ PRODUKCJI
+void S();
+void Z();
+void W();
+void Wprim();
+void P();
+void R();
+void Rprim();
+void L();
+void Lprim();
+void C();
+void O();
 
 //Uchwyty na widgety
 GtkWidget *window;
@@ -32,22 +110,31 @@ GtkWidget *vbox;
 GtkWidget *entry_for_input_expression;
 
 //Funkcje do inicjowania i obsługi widgetów
+
+bool sprawdzWyrazenie(char* expression){
+	reset_data_to_validation();
+	strcpy(BUFFOR,expression); 
+	biezacy_symbol = BUFFOR[indeks];
+	S();
+	int diff = strcmp(BUFFOR_OF_ERROR, EMPTY_STR);
+	if(diff == 0)
+	{
+		return true;
+	}else{
+		return false;
+	}
+}
+
 void validation(GtkWidget *widget, gpointer data) {
   const gchar* expression = gtk_entry_get_text(GTK_ENTRY(entry_for_input_expression));
-	g_print("Expression = %s\n", expression);
-	/*
-	wynik = sprawdzWyrazenie(( char * )expression);
-	g_print("WYNIK WALIDACJI: %d", wynik.isSuccess);
-	g_print("ERROR WALIDACJI: %s", wynik.error);
-	if(wynik.isSuccess)
+	bool isSuccess = sprawdzWyrazenie((char*)expression);
+	if(isSuccess)
 	{
 		gtk_label_set_text(GTK_LABEL(label_on_result_message),SUCCESS_MESSAGE);
-		gtk_label_set_text(GTK_LABEL(label_on_error_message),EMPTY_STR);
-	}else{
+	}else
+	{
 		gtk_label_set_text(GTK_LABEL(label_on_result_message),FAILED_MESSAGE);
-		gtk_label_set_text(GTK_LABEL(label_on_error_message),wynik.error);
 	}
-	*/
 }
 
 GtkWidget* init_window(){
@@ -88,24 +175,429 @@ GtkWidget* init_label_on_error_of_validation(){
   return label_on_error_message;
 }
 
+void reset_data_to_validation(){
+	i = 0;
+	indeks = 0;
+	wyczyscBuffor();
+	wyczyscBufforError();
+	BLAD = false;
+}
+
 void reset_form() {
-	g_print("RESET\n");
+	reset_data_to_validation();
 	gtk_entry_set_text(GTK_ENTRY(entry_for_input_expression),EMPTY_STR);
 	gtk_entry_set_text(GTK_ENTRY(entry_for_input_expression),EMPTY_STR);
 	gtk_label_set_text(GTK_LABEL(label_on_result_message),DEFAULT_RESULT_MESSAGE);
 	gtk_label_set_text(GTK_LABEL(label_on_error_message),DEFAULT_ERROR_MESSAGE);
 }
 
-//PARAMETRY PROGRAMU
-const int ROZMIAR_BUFFORA = 100;
-char BUFFOR[100];
-int indeks = 0;
-char PUSTY_SYMBOL = '\0';
-const int INDEKS_W_TABLICY_ASCI_ZNAKU_NOWEJ_LINI = 10;
-char biezacy_symbol;
-char BLEDNY_SYMBOL = 0;
-bool BLAD = false;
-int i = 0;
+//PONIZEJ ZNAJDUJE SIE IMPLEMENTACJA FUNCKI POMOCNICZYCH
+int rozmiarTablicyZnakow(char **array)
+{
+	return sizeof(array) - 1;
+}
+
+void wyczyscBuffor()
+{
+	for (i = 0; i < ROZMIAR_BUFFORA; i++)
+	{
+		BUFFOR[i] = '\0';
+	}
+}
+
+void wyczyscBufforError()
+{
+	for (i = 0; i < ROZMIAR_BUFFORA; i++)
+	{
+		BUFFOR_OF_ERROR[i] = '\0';
+	}
+}
+
+bool znajdzSymbolWProdukcji(char symbol, int production)
+{
+	char *prod = NULL;
+	int size = 0;
+	if(symbol != INDEKS_W_TABLICY_ASCI_ZNAKU_NOWEJ_LINI)
+	{		
+		switch (production)
+		{
+			case PierwszeProdukcjaS:
+				{
+					prod = FIRST_PRODUCTION_S;
+					size = sizeof(FIRST_PRODUCTION_S);
+					break;
+				}
+			case PierwszeProdukcjaZ:
+				{
+					prod = FIRST_PRODUCTION_Z;
+					size = sizeof(FIRST_PRODUCTION_Z);
+					break;
+				}
+			case PierwszeProdukcjaW:
+				{
+					prod = FIRST_PRODUCTION_W;
+					size = sizeof(FIRST_PRODUCTION_W);
+					break;
+				}
+			case PierwszeProdukcjaP:
+				{
+					prod = FIRST_PRODUCTION_P;
+					size = sizeof(FIRST_PRODUCTION_P);
+					break;
+				}
+			case PierwszeProdukcjaR:
+				{
+					prod = FIRST_PRODUCTION_R;
+					size = sizeof(FIRST_PRODUCTION_R);
+					break;
+				}
+			case PierwszeProdukcjaL:
+				{
+					prod = FIRST_PRODUCTION_L;
+					size = sizeof(FIRST_PRODUCTION_L);
+					break;
+				}
+			case PierwszeProdukcjaC:
+				{
+					prod = FIRST_PRODUCTION_C;
+					size = sizeof(FIRST_PRODUCTION_C);
+					break;
+				}
+			case PierwszeProcukcjaO:
+				{
+					prod = FIRST_PRODUCTION_O;
+					size = sizeof(FIRST_PRODUCTION_O);
+					break;
+				}
+			case PierwszeProdukcjaWprim:
+				{
+					prod = FIRST_PRODUCTION_Wprim;
+					size = sizeof(FIRST_PRODUCTION_Wprim);
+					break;
+				}
+			case PierwszeProdukcjaRprim:
+				{
+					prod = FIRST_PRODUCTION_Rprim;
+					size = sizeof(FIRST_PRODUCTION_Rprim);
+					break;
+				}
+			case PierwszeProdukcjaLprim:
+				{
+					prod = FIRST_PRODUCTION_Lprim;
+					size = sizeof(FIRST_PRODUCTION_Lprim);
+					break;
+				}
+		}
+
+		for (i = 0; i < size; i++)
+		{
+			if (prod[i] == symbol)
+			{
+				return true;
+			}
+		}	
+	}
+	return false;
+}
+
+char nastepnySymbolZInkrementacja()
+{
+	if (indeks + 1 < ROZMIAR_BUFFORA)
+	{
+		indeks++;
+	}
+	else
+	{
+		g_print("Indeks: %d", indeks);
+	}
+	return BUFFOR[indeks];
+}
+
+char nastepnySymbolBezInkrementacja()
+{
+	return BUFFOR[indeks + 1];
+}
+
+void sygnalizujBlad()
+{
+	BLAD = true;
+	BLEDNY_SYMBOL = biezacy_symbol;
+	//g_print("%s", BUFFOR_OF_ERROR);
+}
+
+void wczytaj()
+{
+	biezacy_symbol = nastepnySymbolZInkrementacja();
+	if(BLAD)
+	{
+		sprintf(BUFFOR_OF_ERROR, "Syntax BLAD!");
+		gtk_label_set_text(GTK_LABEL(label_on_error_message),BUFFOR_OF_ERROR);
+	}
+}
+//PONIZEJ ZNAJDUJĄ SIE IMPLEMENTACJE FUNkCJI REALIZUJACYCH POSZCZEGOLNE PRODUKCJE
+
+//PRODUKCJA S
+void S()
+{
+	if (znajdzSymbolWProdukcji(biezacy_symbol, PierwszeProdukcjaW))
+	{
+		W();
+		if (biezacy_symbol != PUSTY_SYMBOL && biezacy_symbol == ';')
+		{
+			wczytaj();
+			Z();
+		}
+		else
+		{
+			if (biezacy_symbol != PUSTY_SYMBOL)
+			{
+				sprintf(BUFFOR_OF_ERROR, "[Blad w produkcji S] Oczekiwano: ';', znaleziono: \'%c\'\n", biezacy_symbol);
+				gtk_label_set_text(GTK_LABEL(label_on_error_message),BUFFOR_OF_ERROR);
+				sygnalizujBlad();
+			}
+		}
+	}
+	else
+	{
+		if (biezacy_symbol != PUSTY_SYMBOL)
+		{
+			sprintf(BUFFOR_OF_ERROR, "[Blad w produkcji S] Oczekiwano: FIRST(W), znaleziono: \'%c\'\n", biezacy_symbol);
+			gtk_label_set_text(GTK_LABEL(label_on_error_message),BUFFOR_OF_ERROR);
+			sygnalizujBlad();
+		}
+	}
+}
+
+//PRODUCKJA Z
+void Z()
+{
+	if (znajdzSymbolWProdukcji(biezacy_symbol, PierwszeProdukcjaW))
+	{
+		W();
+		if ( biezacy_symbol == ';')
+		{
+			wczytaj();
+			if (znajdzSymbolWProdukcji(biezacy_symbol, PierwszeProdukcjaZ))
+			{
+				Z();
+			}
+			else
+			{
+				if (biezacy_symbol != PUSTY_SYMBOL && biezacy_symbol != INDEKS_W_TABLICY_ASCI_ZNAKU_NOWEJ_LINI)
+				{
+					sprintf(BUFFOR_OF_ERROR, "[Blad w produkcji Z] Oczekiwano: FIRST(Z), znaleziono: \'%c\'\n", biezacy_symbol);
+					gtk_label_set_text(GTK_LABEL(label_on_error_message),BUFFOR_OF_ERROR);
+					sygnalizujBlad();
+				}
+			}
+		}
+		else
+		{
+			if (biezacy_symbol != PUSTY_SYMBOL)
+			{
+				sprintf(BUFFOR_OF_ERROR, "[Blad w produkcji Z] Oczekiwano: ';', znaleziono: \'%c\'\n", biezacy_symbol);
+				gtk_label_set_text(GTK_LABEL(label_on_error_message),BUFFOR_OF_ERROR);
+				sygnalizujBlad();
+			}
+		}
+	}
+}
+
+//PRODUKCJA W
+void W()
+{
+	if (znajdzSymbolWProdukcji(biezacy_symbol, PierwszeProdukcjaP))
+	{
+		P();
+		Wprim();
+	}
+	else
+	{
+		if (biezacy_symbol != PUSTY_SYMBOL)
+		{
+			sprintf(BUFFOR_OF_ERROR, "[Blad w produkcji W] Oczekiwano: FIRST(P), znaleziono: \'%c\'\n", biezacy_symbol);
+			gtk_label_set_text(GTK_LABEL(label_on_error_message),BUFFOR_OF_ERROR);
+			sygnalizujBlad();
+		}
+	}
+}
+
+//PRODUKCJA W'
+void Wprim()
+{
+	if (znajdzSymbolWProdukcji(biezacy_symbol, PierwszeProcukcjaO))
+	{
+		O();
+		if (znajdzSymbolWProdukcji(biezacy_symbol, PierwszeProdukcjaW))
+		{
+			W();
+		}
+		else
+		{
+			if (biezacy_symbol != PUSTY_SYMBOL || biezacy_symbol == INDEKS_W_TABLICY_ASCI_ZNAKU_NOWEJ_LINI)
+			{
+				sprintf(BUFFOR_OF_ERROR, "[Blad w produkcji W_PRIM] Oczekiwano: FIRST(W), znaleziono: \'%c\'\n", biezacy_symbol);
+				gtk_label_set_text(GTK_LABEL(label_on_error_message),BUFFOR_OF_ERROR);
+				sygnalizujBlad();
+			}
+		}
+	}
+}
+
+//PRODUKCJA P
+void P()
+{
+	if (znajdzSymbolWProdukcji(biezacy_symbol, PierwszeProdukcjaR))
+	{
+		
+		R();
+	}
+	else if (biezacy_symbol != PUSTY_SYMBOL && biezacy_symbol == '(')
+	{
+		wczytaj();
+		if (znajdzSymbolWProdukcji(biezacy_symbol, PierwszeProdukcjaW))
+		{
+			W();
+			if (biezacy_symbol != PUSTY_SYMBOL && biezacy_symbol == ')')
+			{
+				wczytaj();
+			}
+			else
+			{
+				if (biezacy_symbol != PUSTY_SYMBOL)
+				{
+					sprintf(BUFFOR_OF_ERROR, "[Blad w produkcji P] Oczekiwano: ')', znaleziono: \'%c\'\n", biezacy_symbol);
+					gtk_label_set_text(GTK_LABEL(label_on_error_message),BUFFOR_OF_ERROR);
+					sygnalizujBlad();
+				}
+			}
+		}
+		else
+		{
+			if (biezacy_symbol != PUSTY_SYMBOL)
+			{
+				sprintf(BUFFOR_OF_ERROR, "[Blad w produkcji P] Oczekiwano: FIRST(W), znaleziono: \'%c\'\n", biezacy_symbol);
+				gtk_label_set_text(GTK_LABEL(label_on_error_message),BUFFOR_OF_ERROR);
+				sygnalizujBlad();
+			}
+		}
+	}
+	else
+	{
+		if (biezacy_symbol != PUSTY_SYMBOL)
+		{
+			sprintf(BUFFOR_OF_ERROR, "[Blad w produkcji P] Oczekiwano: FIRST(R) lub '(', znaleziono: \'%c\'\n", biezacy_symbol);
+			gtk_label_set_text(GTK_LABEL(label_on_error_message),BUFFOR_OF_ERROR);
+			sygnalizujBlad();
+		}
+	}
+}
+
+//PRODUKCJA R
+void R()
+{
+	if (znajdzSymbolWProdukcji(biezacy_symbol, PierwszeProdukcjaL))
+	{
+		
+		L();
+		Rprim();
+	}
+	else
+	{
+		if (biezacy_symbol != PUSTY_SYMBOL)
+		{
+			sprintf(BUFFOR_OF_ERROR, "[Blad w produkcji R] Oczekiwano: FIRST(L), znaleziono: \'%c\'\n", biezacy_symbol);
+			gtk_label_set_text(GTK_LABEL(label_on_error_message),BUFFOR_OF_ERROR);
+			sygnalizujBlad();
+		}
+	}
+}
+
+//PRODUKCJA R'
+void Rprim()
+{
+	if (biezacy_symbol != PUSTY_SYMBOL && biezacy_symbol == '.' )
+	{
+		wczytaj();
+		if (znajdzSymbolWProdukcji(biezacy_symbol, PierwszeProdukcjaL))
+		{
+			L();
+		}
+		else
+		{
+			if (biezacy_symbol != PUSTY_SYMBOL )
+			{
+				sprintf(BUFFOR_OF_ERROR, "[Blad w produkcji R'] Oczekiwano: FRIST(L), znaleziono: \'%c\'\n", biezacy_symbol);
+				gtk_label_set_text(GTK_LABEL(label_on_error_message),BUFFOR_OF_ERROR);
+				sygnalizujBlad();
+			}
+		}
+	}
+}
+
+//PRODUKCJA L
+void L()
+{
+	if (znajdzSymbolWProdukcji(biezacy_symbol, PierwszeProdukcjaC))
+	{
+		C();
+		Lprim();
+	}
+	else
+	{
+		if (biezacy_symbol != PUSTY_SYMBOL)
+		{
+			sprintf(BUFFOR_OF_ERROR, "[Blad w produkcji L] Oczekiwano: FIRST(C), znaleziono: \'%c\'\n", biezacy_symbol);
+			gtk_label_set_text(GTK_LABEL(label_on_error_message),BUFFOR_OF_ERROR);
+			sygnalizujBlad();
+		}
+	}
+}
+
+//PRODUKCJA L'
+void Lprim()
+{
+	if (znajdzSymbolWProdukcji(biezacy_symbol, PierwszeProdukcjaL))
+	{
+		L();
+	}
+}
+
+//PRODUKCJA C
+void C()
+{
+	if (znajdzSymbolWProdukcji(biezacy_symbol, PierwszeProdukcjaC)) 
+	{
+		wczytaj();
+	}
+	else
+	{
+		if (biezacy_symbol != PUSTY_SYMBOL)
+		{
+			sprintf(BUFFOR_OF_ERROR, "[Blad w produkcji C] Oczekiwano: cyfra, znaleziono: \'%c\'\n", biezacy_symbol);
+			gtk_label_set_text(GTK_LABEL(label_on_error_message),BUFFOR_OF_ERROR);
+			sygnalizujBlad();
+		}
+	}
+}
+
+//PRODUKCJA O
+void O()
+{
+	if (znajdzSymbolWProdukcji(biezacy_symbol, PierwszeProcukcjaO))
+	{
+		wczytaj();
+	}
+	else
+	{
+		if (biezacy_symbol != PUSTY_SYMBOL)
+		{
+			sprintf(BUFFOR_OF_ERROR, "[Blad w produkcji O] Oczekiwano: operator, znaleziono: \'%c\'\n", biezacy_symbol);
+			gtk_label_set_text(GTK_LABEL(label_on_error_message),BUFFOR_OF_ERROR);
+			sygnalizujBlad();
+		}
+	}
+}
 
 int main(int argc, char *argv[]) {
     
@@ -132,6 +624,8 @@ int main(int argc, char *argv[]) {
   gtk_box_pack_start(GTK_BOX(vbox), label_on_error_message, TRUE, TRUE, 0);
   gtk_box_pack_start(GTK_BOX(vbox), reset_button, TRUE, TRUE, 0);
   
+  wyczyscBuffor();
+	wyczyscBufforError();
   //Przypisanie akcji do widgetów
   g_signal_connect(G_OBJECT(validation_button), "clicked", G_CALLBACK(validation), NULL);
   
@@ -144,3 +638,5 @@ int main(int argc, char *argv[]) {
 
   return 0;
 }
+
+
